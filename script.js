6af8ca17762ce500ad8cbf1d65613c66b7abe798
Larissa Nunes <larissanunes0904@gmail.com>
2023-07-14 00:09:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
